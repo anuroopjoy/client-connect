9c6be111ec4d71a674253edeeac8054d7baa4f14
Pillai <abhilash.pillai@hrblock.com>
2020-06-03 18:26:43 +0530

    micro app in stand alone

--- src/app/micro/micro-client-connect-wrapper.component.ts
+++ src/app/micro/micro-client-connect-wrapper.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
  */
 @Component({
     selector: 'app-micro-client-connect-wrapper',
+    styles:[
+        `
+        .button__wrapper{
+            position: fixed;
+            bottom: 10px;
+            right: 10px;
+            z-index: 11111
+        }
+        `
+    ],
     templateUrl: './micro-client-connect-wrapper.component.html'
 })
 export class MicroClientConnectWrapperComponent {
